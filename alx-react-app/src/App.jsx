affe14b7702b132e86a2eb29463930cbf1cf460a
Deborah Akinbola <akinboladeborah400@yahoo.com>
2025-08-09 07:11:01 -0700

    Add UserProfile component with props functionality

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,16 +1,17 @@
-import Header from './Header'
-import MainContent from './MainContent'
-import Footer from './Footer'
-import './App.css'
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+import UserProfile from './components/UserProfile';
 
 function App() {
-  return (
-    <>
-      <Header />
-      <MainContent />
-      <Footer />
-    </>
-  )
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+            <Footer />
+        </div>
+    );
 }
 
-export default App
+export default App;
