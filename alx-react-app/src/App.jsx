b3182440784de71d05d867103bf3525f0e6452a6
Deborah Akinbola <akinboladeborah400@yahoo.com>
2025-08-09 06:45:23 -0700

    Add Header, MainContent, and Footer components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,19 +1,14 @@
-import { useState } from 'react'
+import Header from './Header'
+import MainContent from './MainContent'
+import Footer from './Footer'
 import './App.css'
-import WelcomeMessage from './components/WelcomeMessage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <WelcomeMessage />
-      <div style={{ marginTop: '20px' }}>
-        <h2>Vite + React</h2>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <Header />
+      <MainContent />
+      <Footer />
     </>
   )
 }
